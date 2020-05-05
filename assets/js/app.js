80518dcceb136e1de48aa5c96cee9860769efbc1
Tha Gidi <you@example.com>
2020-05-05 01:03:40 +0100

    nav, footer and hero built

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,5 @@
 
 // JavaScript
-window.sr = ScrollReveal(); 
-
-// sr.reveal('h1', {
-//     delay: 0,
-//     duration: 200,
-//     origin: 'bottom',
-//     distance: '100px' 
+// $(document).ready(function(){
+//     $('.carousel').carousel('pause');
 // });
