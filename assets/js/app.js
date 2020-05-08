b03c7fdb5ab91fe51822ed1d18e5dde23cc179ef
Tha Gidi <you@example.com>
2020-05-08 22:59:49 +0100

    js removed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,5 @@
 
 // JavaScript
-$(document).ready(function(){
-    $('.carousel').carousel('pause');
-    $('.menu-toggle').click(function(){
-        $('.responsive-nav').toggleClass("unfold");
-        $(this).toggleClass("close-menu");
-    });
-
-    $('#responsive-nav, .unfold.nav-links').click(function(){
-        $('.responsive-nav').toggleClass("unfold");
-        $('.menu-toggle').toggleClass("close-menu");
-    });
-});
+// $(document).ready(function(){
+//     $('.carousel').carousel('pause');
+// });
