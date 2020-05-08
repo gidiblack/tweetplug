90cde1a457d90aa674c983acf4fcfd3931939812
Tha Gidi <you@example.com>
2020-05-08 22:33:53 +0100

    navbar now responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,13 @@
 // JavaScript
 $(document).ready(function(){
     $('.carousel').carousel('pause');
+    $('.menu-toggle').click(function(){
+        $('.responsive-nav').toggleClass("unfold");
+        $(this).toggleClass("close-menu");
+    });
+
+    $('#responsive-nav, .unfold.nav-links').click(function(){
+        $('.responsive-nav').toggleClass("unfold");
+        $('.menu-toggle').toggleClass("close-menu");
+    });
 });
