03a94fa27838ee47b006c1abc05ec74152f794a9
Tha Gidi <you@example.com>
2020-06-16 03:02:33 +0100

    admin dashboard added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 
 // JavaScript
-// $(document).ready(function(){
-//     $('.carousel').carousel('pause');
-// });
+// datatables plugin gotten from https://datatables.net/examples/styling/bootstrap4
+
+$(document).ready(function() {
+    // Datatable plugin call
+    $('#influencers').DataTable();
+    $('#withdrawalRequests').DataTable();
+} );
