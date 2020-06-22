73405f08c5c898eb5b0188000e12f824c8ec2189
mofe <ogunbiyioladapo33@gmail.com>
2020-06-22 02:18:06 -0700

    authentication and error handling fix

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const methodOverride = require('method-override');
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
 
+const userRouter = require('./routers/userRoutes');
+
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +16,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 app.use(express.json({ limit: '10kb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
-app.use(cookieParser);
+app.use(cookieParser());
+
+app.use('/api/v1/user', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`));
+});
+
+//errors
+app.use(globalErrorHandler);
 
 module.exports = app;
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,14 +6,17 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
-  const message = `Duplicate field value:${value}. Please use another value `;
+  //console.log(err.keyValue);
+  const key = Object.keys(err.keyPattern);
+  const value = Object.values(err.keyValue);
+  const message = `${key} : ${value}, already Exists, Please try another`;
   return new AppError(message, 400);
 };
 
 const handleValidationErrorDB = (err) => {
   const error = Object.values(err.errors).map((el) => el.message);
-  const message = `Invalid input Data. ${error.join('. ')}`;
+  //console.log(error);
+  const message = `Invalid input Data. ${error.join(', ')}`;
   return new AppError(message, 400);
 };
 
@@ -24,58 +27,27 @@ const handleJWTExpiredError = () =>
   new AppError('Your token has expired, Please log in again', 401);
 
 const sendErrorDev = (err, req, res) => {
-  //for API
-  if (req.originalUrl.startsWith('/api')) {
-    return res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    });
-  }
-  //for rendered site
-  //console.log(err);
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong',
-    msg: err.message,
-    statusCode: err.statusCode,
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
   });
 };
 
 const sendErrorProd = (err, req, res) => {
-  //for API
-  if (req.originalUrl.startsWith('/api')) {
-    //operational errors
-    if (err.isOperational) {
-      return res.status(err.statusCode).json({
-        status: err.status,
-
-        message: err.message,
-      });
-    }
-    //internal errors
-    console.log(err);
-    return res.status(500).json({
-      status: 'error',
-      message: `Looks like Something went wrong, Please don't hate us  `,
-    });
-  }
-  // for rendered website
   if (err.isOperational) {
-    //operational errors
-    return res.status(err.statusCode).render('error', {
-      title: 'Something went wrong',
-      msg: err.message,
-      statusCode: err.statusCode,
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('Error', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Looks like something went wrong',
     });
   }
-  console.log(err);
-  //Internal error
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong',
-    msg: 'Welp! Looks like Mofe messed up again. Please try again later',
-    statusCode: err.statusCode,
-  });
 };
 
 module.exports = (err, req, res, next) => {
@@ -83,10 +55,16 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
+    //console.log(err.name);
     sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
+    //console.log(err.name);
+
     let error = { ...err };
     error.message = err.message;
+    error.name = err.name;
+    //error.statusCode = err.statusCode;
+
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldDB(error);
     if (error.name === 'ValidationError')
